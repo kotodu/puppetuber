5f722ef79ea117cb61b653fe4773e5215f797e7b
kotodu <kotokotaro@gmail.com>
2025-04-18 23:38:41 +0900

    add clean command and fix config

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
 import puppeteer from "puppeteer";
 // Or import puppeteer from 'puppeteer-core';
 
+const baseURL = "https://www.youtube.com/watch?v=";
+const videoID = "iYFExyAsEYs";
+const time = 2;
+const videoURL = `${baseURL}${videoID}&t=${time}`;
+
 const browseTest = async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Navigate the page to a URL.
-  await page.goto("https://developer.chrome.com/");
+  await page.goto(videoURL);
 
   // Set screen size.
-  await page.setViewport({ width: 1080, height: 1024 });
+  await page.setViewport({ width: 1280, height: 720 });
 
   page.screenshot({
-    path: "./screenshot.png",
+    path: "./screenshots/screenshot.png",
   });
 
   // Type into search box.
-  await page.locator(".devsite-search-field").fill("automate beyond recorder");
+  //   await page.locator(".devsite-search-field").fill("automate beyond recorder");
 
   // Wait and click on first result.
-  await page.locator(".devsite-result-item-link").click();
+  //   await page.locator(".devsite-result-item-link").click();
 
   // Locate the full title with a unique string.
-  const textSelector = await page
-    .locator("text/Customize and automate")
-    .waitHandle();
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+  //   const textSelector = await page
+  //     .locator("text/Customize and automate")
+  //     .waitHandle();
+  //   const fullTitle = await textSelector?.evaluate((el) => el.textContent);
 
   // Print the full title.
-  console.log('The title of this blog post is "%s".', fullTitle);
+  //   console.log('The title of this blog post is "%s".', fullTitle);
 
   await browser.close();
 };
