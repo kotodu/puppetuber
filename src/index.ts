5385ce14c8f6618c666699faff57347b608f4c00
kotodu <kotokotaro@gmail.com>
2025-05-01 22:48:25 +0900

    add iteration of screenshot

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import puppeteer from "puppeteer";
 const baseURL = "https://www.youtube.com/watch?v=";
 const videoID = "iYFExyAsEYs";
 const time = 20;
-const videoURL = `${baseURL}${videoID}&t=${time}`;
+const videoURL = `${baseURL}${videoID}`;
 
 const browseYouTube = async () => {
   // ヘッドレスモードを無効化してブラウザを表示
@@ -21,22 +21,28 @@ const browseYouTube = async () => {
   await page.screenshot({ path: "./screenshots/screenshot0.png" });
   console.log("Screenshot taken");
 
-  // 動画の再生ボタンをクリック
-  const playButtonSelector = ".ytp-large-play-button";
-  await page.waitForSelector(playButtonSelector);
-  await page.click(playButtonSelector);
+  // 動画の再生ボタンをクリックしてそのまま停止
+  await page.keyboard.press("Space", {
+    delay: 1000,
+  });
   console.log("Video started");
+  await page.keyboard.press("Space", {
+    delay: 1000,
+  });
 
-  // フルスクリーンボタンをクリック
-  const fullScreenButtonSelector = ".ytp-fullscreen-button";
-  await page.click(fullScreenButtonSelector);
+  // フルスクリーンモードに切り替え
+  await page.keyboard.press("f", {
+    delay: 2000,
+  });
   console.log("Fullscreen activated");
 
   await page.screenshot({ path: "./screenshots/screenshot1.png" });
   console.log("Screenshot taken");
 
-  await page.screenshot({ path: "./screenshots/screenshot2.png" });
-  console.log("Screenshot taken");
+  for (let i = 10; i < 15; i++) {
+    await page.keyboard.press("ArrowRight", { delay: 1000 });
+    await page.screenshot({ path: `./screenshots/screenshot${i}.png` });
+  }
 
   // 動画を一時停止
   const pauseButtonSelector = ".ytp-play-button";
